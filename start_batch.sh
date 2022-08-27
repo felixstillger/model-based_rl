669fbfb10f8948da98c0e80bf29896520e70ad5c
fs608798 <fs608798@login18-x-1.hpc.itc.rwth-aachen.de>
2022-08-27 19:52:32 +0200

    updated batch file; trainings py update

---
+++ start_batch.sh
@@ -0,0 +1,22 @@
+#!/usr/local_rwth/bin/zsh
+ 
+### Job name
+
+#SBATCH --mail-user=felix.stillger@rwth-aachen.de
+#SBATCH --mail-type=ALL
+
+#SBATCH -J cpu_serial
+#SBATCH -o logs/cpu_serial.%J.log
+#SBATCH --ntasks-per-node=4
+#SBATCH --mem-per-cpu=15G
+#SBATCH --time=0-06:30:00
+# Load the same python as used for installation
+
+
+module load python/3.9.6
+
+# start skript
+python3 --version
+# python3 /home/fs608798/seminararbeit/progra/Mask_RCNN/just_test.py
+python3 Alphazero_Train.py
+
