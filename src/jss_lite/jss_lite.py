53cc167503218e802caf244437fb4563348506b3
felix.stillger <felix.stillger@rwth-aachen.de>
2022-09-02 12:11:45 +0200

    new lite environment

---
+++ src/jss_lite/jss_lite.py
@@ -0,0 +1,21 @@
+import pandas as pd
+import gym
+import numpy as np
+import plotly.figure_factory as ff
+from pathlib import Path
+
+
+class jss_lite(gym.Env):
+
+    def __init__(self, env_config=None):
+        pass
+    def step(self,action):
+        pass
+    def reset(self):
+        pass
+    def get_state():
+        pass
+    def set_state():
+        pass
+    def render():
+        pass
