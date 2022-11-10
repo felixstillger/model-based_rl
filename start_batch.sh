d78537f46557e649ac0e58bb6436f3824a35c8a6
felix.stillger <felix.stillger@rwth-aachen.de>
2022-11-10 07:55:41 +0100

    training for 8x8x8

--- start_batch.sh
+++ start_batch.sh
@@ -8,7 +8,7 @@
 #SBATCH -J cpu_serial
 #SBATCH -o logs/cpu_serial.%J.log
 #SBATCH --ntasks-per-node=4
-#SBATCH --mem-per-cpu=15G
+#SBATCH --mem-per-cpu=5G
 #SBATCH --time=0-04:30:00
 # Load the same python as used for installation
 
@@ -18,5 +18,5 @@ module load python/3.9.6
 # start skript
 python3 --version
 # python3 /home/fs608798/seminararbeit/progra/Mask_RCNN/just_test.py
-python3 eval.py
+python3 train_multiinstance.py
 
--- train_multiinstance.py
+++ train_multiinstance.py
@@ -49,9 +49,16 @@ def main():
     ray.shutdown()
     curr_dir=(os.path.dirname(__file__))
 
-    instances_names='ima_6_6_no_act'
+    num_inst=str(6)
+    instances_names='ima_'+num_inst+'_'+num_inst+'_no_act'
     saving_directory='/training_checkpoints/'+instances_names
+    ima_inst_train=[]
+    ima_inst_test=[]
 
+    for i in range(0,20):
+        ima_inst_train.append(curr_dir+'/resources/jsp_instances/ima/'+num_inst+'x'+num_inst+'x'+num_inst+'/'+num_inst+'x'+num_inst+'_'+str(i)+'_inst.json')
+    for i in range(21,30):
+        ima_inst_test.append(curr_dir+'/resources/jsp_instances/ima/'+num_inst+'x'+num_inst+'x'+num_inst+'/'+num_inst+'x'+num_inst+'_'+str(i)+'_inst.json')
     #check savin directory
 
     #curr_dir='/Users/felix/sciebo/masterarbeit/progra/model-based_rl'
@@ -64,14 +71,6 @@ def main():
     huge_inst_train=["la01","la02","la03","la16","la17","la18","la06","la07","la08","la09","la21","la22","la23","la24","la25","la36","la37","ta01","ta02","ta03","ta04","ta05","ta06","ta07","abz7","abz9","la26","la27","la28"]
     huge_inst_test=["la04","la05","la19","la20","la10","la38","la39","la40","ta08","ta09","ta10","la29","la30"]
     #huge_inst_train=["la01","la02","la03","la16","la17","la18","la21","la22","la23","la24","la25","la36","la37","ta01","ta02","ta03","ta04","ta05","ta06","ta07","abz7","abz9","la26","la27","la28"]
-    
-    ima_inst_train=[]
-    ima_inst_test=[]
-    num=str(6)
-    for i in range(0,20):
-        ima_inst_train.append(curr_dir+'/resources/jsp_instances/ima/'+num+'x'+num+'x'+num+'/'+num+'x'+num+'_'+str(i)+'_inst.json')
-    for i in range(21,30):
-        ima_inst_test.append(curr_dir+'/resources/jsp_instances/ima/'+num+'x'+num+'x'+num+'/'+num+'x'+num+'_'+str(i)+'_inst.json')
 
     # add current directory to path
     instance_list_training=[curr_dir + s for s in instance_list_training]
@@ -102,11 +101,11 @@ def main():
             os.mkdir(s_path)
 
 
-    train_agent=False
+    train_agent=True
     eval_agent=True
     restore_agent= False
-    num_episodes = 3
-    inner_episodes=5
+    num_episodes = 4
+    inner_episodes=1
     config = {
         "framework": "torch",
         "disable_env_checking":True,
@@ -223,62 +222,37 @@ def main():
 
         return time.time()-t,reward,episode.length
 
-
     if eval_agent:
         eval_result={}
         #agent = AlphaZeroTrainer( config=config_eval, env='custom_jssp')
         for _ in range(num_episodes):
-
             # here goes the training instances:
             for instance in instance_list_training + instance_list_validation:
                 reload_ray_validation([instance])
                 agent = AlphaZeroTrainer( config=config_eval, env='custom_jssp')
-                #for checkpoints in instance_list_training:
-                #checkpoints='/Users/felix/sciebo/masterarbeit/progra/model-based_rl/training_checkpoints/cluster_data/training_checkpoints/la_multi_huge/ta07.txt'
-                checkpoints='/Users/felix/sciebo/masterarbeit/progra/model-based_rl/resources/jsp_instances/ima/6x6x6/6x6_19_inst.json'
-                #eval_path=curr_dir+'/training_checkpoints/la_multi'+"/"+instance_list_training[-1][-8:-4]+"/"+str(_)
-                #episode=_
-                episode=2
-                eval_path=curr_dir+saving_directory+"/"+get_instance_name(checkpoints)+"/"+str(_)
-                if not os.path.exists(eval_path):
-                    #results=pd.DataFrame.from_dict(eval_result,orient='index')
-                    #results.to_csv('results.csv')
-                    #print(f"{eval_path} does not exits: break")
-                    return True
-                else:
-                    agent.load_checkpoint(eval_path+"/checkpoint-1")
-                    print(f"loaded: {eval_path}")
-                    #eval_env= jssp_light_obs_wrapper_multi_instances([instance])
-                    e_t=time.time()
-                    eval_env=env_creator_variable_instance(config="config",env_name=instance)
-                    eval_tmp={}
-                    eval_tmp["time"],eval_tmp["reward"],eval_tmp["length"] = eval_agent_on_instance(agent,eval_env)
-                    eval_tmp["instance"]=get_instance_name(instance)
-                    eval_tmp['episode']=_
-                    eval_tmp['checkpoint']=get_instance_name(checkpoints)+"/"+str(_)
-                    eval_result[str(_),get_instance_name(checkpoints),get_instance_name(instance)]=eval_tmp
-                    results=pd.DataFrame.from_dict(eval_result,orient='index')
-                    results.to_csv('results.csv')
-                    print(f"{instance} in time: {time.time()-e_t}")
-                # here goes the validation:
-                # for instance in instance_list_validation:
-                #     reload_ray_validation([instance])
-                #     agent = AlphaZeroTrainer( config=config_eval, env='custom_jssp')
-                #     agent.load_checkpoint(eval_path+"/checkpoint-1")
-                #     eval_env= jssp_light_obs_wrapper_multi_instances([instance])
-                #     e_t,e_reward,e_length = eval_agent_on_instance(agent,eval_env)
-                #     eval_tmp={}
-                #     eval_tmp["time"]=e_t
-                #     eval_tmp["reward"]=e_reward
-                #     eval_tmp["length"]=e_length
-                #     eval_tmp["instance"]=get_instance_name(instance)
-                #     eval_tmp['episode']=_
-                #     eval_tmp['checkpoint']=get_instance_name(checkpoints)+"/"+str(_)
-                #     eval_result[str(_),get_instance_name(checkpoints),get_instance_name(instance)]=eval_tmp
-                #     results=pd.DataFrame.from_dict(eval_result,orient='index')
-                #     results.to_csv('results'+instances_names+'.csv')
-                #     print(f"{instance} in time: {e_t}")
-
+                for checkpoints in instance_list_training:
+                    #checkpoints='/Users/felix/sciebo/masterarbeit/progra/model-based_rl/training_checkpoints/cluster_data/training_checkpoints/la_multi_huge/ta07.txt'
+                    eval_path=curr_dir+saving_directory+"/"+get_instance_name(checkpoints)+"/"+str(_)
+                    if not os.path.exists(eval_path):
+                        #results=pd.DataFrame.from_dict(eval_result,orient='index')
+                        #results.to_csv('results.csv')
+                        #print(f"{eval_path} does not exits: break")
+                        return True
+                    else:
+                        agent.load_checkpoint(eval_path+"/checkpoint-1")
+                        print(f"loaded: {eval_path}")
+                        #eval_env= jssp_light_obs_wrapper_multi_instances([instance])
+                        e_t=time.time()
+                        eval_env=env_creator_variable_instance(config="config",env_name=instance)
+                        eval_tmp={}
+                        eval_tmp["time"],eval_tmp["reward"],eval_tmp["length"] = eval_agent_on_instance(agent,eval_env)
+                        eval_tmp["instance"]=get_instance_name(instance)
+                        eval_tmp['episode']=_
+                        eval_tmp['checkpoint']=get_instance_name(checkpoints)+"/"+str(_)
+                        eval_result[str(_),get_instance_name(checkpoints),get_instance_name(instance)]=eval_tmp
+                        results=pd.DataFrame.from_dict(eval_result,orient='index')
+                        results.to_csv('results'+instances_names+'.csv')
+                        print(f"{instance} in time: {time.time()-e_t}")
     
 if __name__ == '__main__':
     sys.exit(main())
